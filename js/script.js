d508f268f3975de676db2392cdf1bd8dcf900fa6
jua850623 <freejuaa@naver.com>
2023-08-18 12:16:33 +0900

    보그

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(".berger").click(
+    function(){
+        $(this).toggleClass("on");
+        $(".popup_nav").toggleClass("on");
+        $(".gnb_1").toggleClass("on");
+        console.log(1111);
+    }
+);
